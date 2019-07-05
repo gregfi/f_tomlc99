883a8bdd1e781dbf7b13456131cad46bbebcf7fd
Greg Fischer <greg.a.fischer@gmail.com>
2019-07-05 15:37:08 -0400

    parsing seems to work

--- f_tomlc99.f90
+++ f_tomlc99.f90
@@ -1,5 +1,10 @@
 module f_tomlc99
-  use iso_c_binding, only: c_int, c_ptr, c_char, c_null_char
+  use, intrinsic :: iso_fortran_env, only : stdin=>input_unit, &
+                                            stdout=>output_unit, &
+                                            stderr=>error_unit
+  use iso_c_binding, only: c_ptr, c_char, c_null_char, c_int, c_size_t, &
+                           c_associated
+
   implicit none
 
   interface
@@ -10,10 +15,18 @@ module f_tomlc99
       character(kind=c_char) :: fileName, mode
     end function
 
+    function fclose(filePtr) bind(C)
+      import                 :: c_ptr, c_int
+      type(c_ptr), value     :: filePtr
+      integer(c_int)         :: fclose
+    end function
+
     function toml_parse_file(filePtr, errBuf, errBufSz) bind(C)
-      import         :: c_ptr, c_int
-      type(c_ptr)    :: toml_parse_file, filePtr, errBuf
-      integer(c_int) :: errBufSz 
+      import                 :: c_ptr, c_char, c_int
+      type(c_ptr)            :: toml_parse_file
+      type(c_ptr), value     :: filePtr
+      character(kind=c_char) :: errBuf(*)
+      integer(c_int)         :: errBufSz 
     end function 
 
   end interface
@@ -22,7 +35,43 @@ module f_tomlc99
 
   subroutine open_toml_file(fileName)
     character(len=*), intent(in) :: fileName
+    type(c_ptr) :: fh, tomlPtr
+    character(len=512, kind=c_char):: errBuf
 
+    fh = fopen(trim(fileName) // c_null_char, &
+               c_char_"r" // c_null_char)
+
+    if (c_associated(fh) .eqv. .false.) then
+      write(stderr,101) trim(fileName)
+      error stop
+    endif
+
+    tomlPtr = toml_parse_file(fh, errBuf, len(errBuf, kind=c_int))
+
+    if (c_associated(tomlPtr) .eqv. .false.) then
+      write(stderr,102) trim(fileName)
+      call write_error_buffer(errBuf)
+      error stop
+    endif
+   
+    if (fclose(fh) /= 0) then
+      write(stderr,103) trim(fileName)
+      error stop
+    endif
+
+    101 format ('ERROR: Failed to open ',a)
+    102 format ('ERROR: Failed to parse ',a)
+    103 format ('ERROR: Failed to close ',a)
+
+  end subroutine
+
+  subroutine write_error_buffer(errBuf)
+    character(len=*, kind=c_char), intent(in) :: errBuf
+    integer :: idx
+    do idx=1,len(errBuf)
+      if (errBuf(idx:idx) == char(0)) exit
+    enddo
+    write(stderr, '(a)') errBuf(1:idx)
   end subroutine
 
 end module
