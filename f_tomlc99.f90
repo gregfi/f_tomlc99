a1e2331aab5f7ae2f659f99c6b5512a7c3647f13
Greg Fischer <greg.a.fischer@gmail.com>
2019-07-05 20:02:05 -0400

    more interfaces

--- f_tomlc99.f90
+++ f_tomlc99.f90
@@ -2,9 +2,11 @@ module f_tomlc99
   use, intrinsic :: iso_fortran_env, only : stdout=>output_unit, &
                                             stderr=>error_unit
   use iso_c_binding, only: c_ptr, c_char, c_null_char, c_int, &
-                           c_associated
-
+                           c_associated, c_null_ptr
   implicit none
+
+  type(c_ptr), protected :: toml_data = c_null_ptr, &
+                            toml_table = c_null_ptr
 
   interface
 
@@ -28,13 +30,53 @@ module f_tomlc99
       integer(c_int)         :: errBufSz 
     end function 
 
+    function toml_table_nkval(tblPtr) bind(C)
+      import                 :: c_ptr, c_int
+      type(c_ptr), value     :: tblPtr
+      integer(c_int)         :: toml_table_nkval
+    end function 
+
+    function toml_table_narr(tblPtr) bind(C)
+      import                 :: c_ptr, c_int
+      type(c_ptr), value     :: tblPtr
+      integer(c_int)         :: toml_table_nkval
+    end function 
+
+    function toml_table_ntab(tblPtr) bind(C)
+      import                 :: c_ptr, c_int
+      type(c_ptr), value     :: tblPtr
+      integer(c_int)         :: toml_table_nkval
+    end function 
+
+    function toml_raw_in(dataPtr, keyName) bind(C)
+      import                 :: c_ptr, c_char, c_int
+      type(c_ptr)            :: toml_raw_in
+      type(c_ptr), value     :: dataPtr
+      character(kind=c_char) :: keyName(*)
+    end function 
+
+    function toml_table_in(dataPtr, tableName) bind(C)
+      import                 :: c_ptr, c_char, c_int
+      type(c_ptr)            :: toml_table_in
+      type(c_ptr), value     :: dataPtr
+      character(kind=c_char) :: tableName(*)
+    end function 
+
+    function toml_array_in(dataPtr, arrayName) bind(C)
+      import                 :: c_ptr, c_char, c_int
+      type(c_ptr)            :: toml_array_in
+      type(c_ptr), value     :: dataPtr
+      character(kind=c_char) :: arrayName(*)
+    end function 
+
+
   end interface
 
   contains
 
   subroutine open_toml_file(fileName)
     character(len=*), intent(in) :: fileName
-    type(c_ptr) :: fh, tomlPtr
+    type(c_ptr) :: fh
     character(len=512, kind=c_char):: errBuf
 
     fh = fopen(trim(fileName) // c_null_char, &
@@ -45,9 +87,9 @@ module f_tomlc99
       error stop
     endif
 
-    tomlPtr = toml_parse_file(fh, errBuf, len(errBuf, kind=c_int))
+    toml_data = toml_parse_file(fh, errBuf, len(errBuf, kind=c_int))
 
-    if (c_associated(tomlPtr) .eqv. .false.) then
+    if (c_associated(toml_data) .eqv. .false.) then
       write(stderr,102) trim(fileName)
       call write_error_buffer(errBuf)
       error stop
@@ -71,6 +113,23 @@ module f_tomlc99
       if (errBuf(idx:idx) == char(0)) exit
     enddo
     write(stderr, '(a)') errBuf(1:idx)
+  end subroutine
+
+  subroutine toml_load_table(tblName, ierr)
+    character(len=*), intent(in)  :: tblName
+    integer,          intent(out) :: ierr
+
+    ierr = 0
+    toml_table = toml_table_in(toml_data, &
+                               tblName // c_null_char)
+
+    if (c_associated(toml_data) .eqv. .false.) then
+      write(stderr,101) trim(tblName)
+      ierr = 1
+    endif
+ 
+    101 format ('ERROR: Failed to find table: ',a)
+
   end subroutine
 
 end module
